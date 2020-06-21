2bf2b77129045324df8247e8ab585f32ab7170bf
Ji Zhi Qian <jizhi.qian@jimubox.com>
2020-06-21 23:23:07 +0800

    Clear environment

--- deploy/deploy_tomcat_pipeline.groovy
+++ deploy/deploy_tomcat_pipeline.groovy
@@ -12,44 +12,61 @@ stage('maven编译打包') {
         def mvnHome = tool 'maven-3.6.0_master'
         env.PATH = "${mvnHome}/bin:${env.PATH}"
         sh '''
-        export pwd=`pwd`
-        export os_type=`uname`
-        cd web/src/main/resources/config
-        if [[ "${os_type}" == "Darwin" ]]; then
-            sed -i "" "s/mysql_ip/${mysql_ip}/g" application.yml
-            sed -i "" "s/mysql_port/${mysql_port}/g" application.yml
-            sed -i "" "s/mysql_user/${mysql_user}/g" application.yml
-            sed -i "" "s/mysql_pwd/${mysql_pwd}/g" application.yml
-        else
-            sed -i "s/mysql_ip/${mysql_ip}/g" application.yml
-            sed -i "s/mysql_port/${mysql_port}/g" application.yml
-            sed -i "s/mysql_user/${mysql_user}/g" application.yml
-            sed -i "s/mysql_pwd/${mysql_pwd}/g" application.yml
-        fi
-        cd $pwd/web
-        mvn clean package spring-boot:repackage -Dmaven.test.skip=true -U
+            export pwd=`pwd`
+            export os_type=`uname`
+            cd web/src/main/resources/config
+            if [[ "${os_type}" == "Darwin" ]]; then
+                sed -i "" "s/mysql_ip/${mysql_ip}/g" application.yml
+                sed -i "" "s/mysql_port/${mysql_port}/g" application.yml
+                sed -i "" "s/mysql_user/${mysql_user}/g" application.yml
+                sed -i "" "s/mysql_pwd/${mysql_pwd}/g" application.yml
+            else
+                sed -i "s/mysql_ip/${mysql_ip}/g" application.yml
+                sed -i "s/mysql_port/${mysql_port}/g" application.yml
+                sed -i "s/mysql_user/${mysql_user}/g" application.yml
+                sed -i "s/mysql_pwd/${mysql_pwd}/g" application.yml
+            fi
+            cd $pwd/web
+            mvn clean package spring-boot:repackage -Dmaven.test.skip=true -U
         '''
     }
 }
 
-stage('kill tomcat') {
+stage('停止 tomcat') {
     node('master'){
         sh '''
-        ## 停止tomcat的函数, 参数$1带入tomcat的路径$TOMCAT_PATH
-        killTomcat()
-        {
-            pid=`ps -ef|grep $1|grep java|awk '{print $2}'`
-            echo "tomcat Id list :$pid"
-            if [ "$pid" = "" ]
-            then
-              echo "no tomcat pid alive"
-            else
-              kill -9 $pid
-            fi
-        }
-        ## 停止Tomcat
-        killTomcat $tomcat_home
+            ## 停止tomcat的函数, 参数$1带入tomcat的路径$TOMCAT_PATH
+            killTomcat()
+            {
+                pid=`ps -ef|grep $1|grep java|awk '{print $2}'`
+                echo "tomcat Id list :$pid"
+                if [ "$pid" = "" ]
+                then
+                  echo "no tomcat pid alive"
+                else
+                  kill -9 $pid
+                fi
+            }
+            ## 停止Tomcat
+            killTomcat $tomcat_home
+        '''
+    }
+}
 
+stage('清理环境') {
+    node('master'){
+        sh '''
+            ## 删除原有war包
+            rm -f $tomcat_home/webapps/ROOT.war
+            rm -rf $tomcat_home/webapps/ROOT
+        '''
+    }
+}
+
+stage('部署新的war包') {
+    node('master'){
+        sh '''
+            echo ${env.WORKSPACE}
         '''
     }
 }
