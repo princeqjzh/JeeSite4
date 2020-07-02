f52d1b098a667fce1645f27ee8480c3aed6a93f9
Ji Zhi Qian <jizhi.qian@jimubox.com>
2020-07-02 16:25:51 +0800

    Script pipeline tomcat

---
+++ deploy/deploy_tomcat_script_pipeline.groovy
@@ -0,0 +1,85 @@
+stage('同步源码') {
+    node('master'){
+        git([url: 'git@github.com:princeqjzh/JeeSite4.git', branch: '${branch}'])
+    }
+}
+
+stage('maven编译打包') {
+    node('master'){
+        sh '''
+            . ~/.bash_profile
+            
+            export pwd=`pwd`
+            export os_type=`uname`
+            cd web/src/main/resources/config
+            if [[ "${os_type}" == "Darwin" ]]; then
+                sed -i "" "s/mysql_ip/${mysql_ip}/g" application.yml
+                sed -i "" "s/mysql_port/${mysql_port}/g" application.yml
+                sed -i "" "s/mysql_user/${mysql_user}/g" application.yml
+                sed -i "" "s/mysql_pwd/${mysql_pwd}/g" application.yml
+            else
+                sed -i "s/mysql_ip/${mysql_ip}/g" application.yml
+                sed -i "s/mysql_port/${mysql_port}/g" application.yml
+                sed -i "s/mysql_user/${mysql_user}/g" application.yml
+                sed -i "s/mysql_pwd/${mysql_pwd}/g" application.yml
+            fi
+            
+            cd $pwd/root
+            mvn clean install -Dmaven.test.skip=true
+            
+            cd $pwd/web
+            mvn clean package spring-boot:repackage -Dmaven.test.skip=true -U
+        '''
+    }
+}
+
+stage('停止 tomcat') {
+    node('master'){
+        sh '''
+            ## 停止tomcat的函数, 参数$1带入tomcat的路径$TOMCAT_PATH
+            killTomcat()
+            {
+                pid=`ps -ef|grep $1|grep java|awk '{print $2}'`
+                echo "tomcat Id list :$pid"
+                if [ "$pid" = "" ]
+                then
+                  echo "no tomcat pid alive"
+                else
+                  kill -9 $pid
+                fi
+            }
+            ## 停止Tomcat
+            killTomcat $tomcat_home
+        '''
+    }
+}
+
+stage('清理环境') {
+    node('master'){
+        sh '''
+            ## 删除原有war包
+            rm -f $tomcat_home/webapps/ROOT.war
+            rm -rf $tomcat_home/webapps/ROOT
+        '''
+    }
+}
+
+stage('部署新的war包') {
+    node('master'){
+        sh '''
+            cp web/target/web.war $tomcat_home/webapps/
+            cd $tomcat_home/webapps
+            mv web.war ROOT.war
+        '''
+    }
+}
+
+stage('启动tomcat') {
+    node('master'){
+        sh '''
+            JENKINS_NODE_COOKIE=dontkillme
+            cd $tomcat_home/bin
+            sh startup.sh
+        '''
+    }
+}
